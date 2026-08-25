2fc8179b1a1cda57c00ef3171f40f4fa39813860
Justin-K-Ellis <jlke45@gmail.com>
2026-08-25 11:26:40 +0900

    chore: move tech stack info into separate file

---
+++ src/content/myTechStack.ts
@@ -0,0 +1,21 @@
+export const progLanguages = ["Python", "TypeScript/JavaScript", "SQL"];
+
+export const backendTech = [
+  "FastAPI",
+  "pandas, polars",
+  "PostgreSQL",
+  "OpenAI SDK",
+  "Node.js",
+  "Express.js",
+];
+
+export const frontendTech = [
+  "React",
+  "React Router",
+  "Next.js",
+  "Tailwind CSS",
+  "daisyUI",
+  "Astro",
+];
+
+export const tooling = ["Git, GitHub", "Databricks", "Vim", "Vite", "Notion"];
